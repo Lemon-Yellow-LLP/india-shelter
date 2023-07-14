c0af1b9f5bb045f4f1f271e170ae8dd3228f39f5
ShUbHaM13M <shubham.heeralal@gmail.com>
2023-07-14 18:50:18 +0530

    Updated the API URL

--- src/global/index.js
+++ src/global/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://scotttiger.in/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://agile.indiashelter.in/api';
 const API_LEAD_URL = `${API_URL}/lead`;
 
 const requestOptions = {};
